d9bcd5792bf29a90f6e2070a6eec5aa560aebef8
unknown <Templerbai@gmail.com>
2020-11-22 16:27:41 +0800

    first commit

---
+++ src/main/java/server/TestServer2.java
@@ -0,0 +1,22 @@
+package server;
+
+
+import service.BlogService;
+import service.BlogServiceImpl;
+import service.UserService;
+import service.UserServiceImpl;
+
+public class TestServer2 {
+    public static void main(String[] args) {
+        UserService userService = new UserServiceImpl();
+        BlogService blogService = new BlogServiceImpl();
+
+        ServiceProvider serviceProvider = new ServiceProvider("127.0.0.1", 8900);
+        // System.out.println("hahah");
+        serviceProvider.provideServiceInterface(userService);
+        serviceProvider.provideServiceInterface(blogService);
+        RPCServer RPCServer = new NettyRPCServer(serviceProvider);
+
+        RPCServer.start(8900);
+    }
+}
